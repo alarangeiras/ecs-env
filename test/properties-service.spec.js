01e359e9dbfa6d3dfb4d51be1b2ce48bc75560b4
Allan Larangeiras <alarangeiras@gmail.com>
2021-02-15 18:47:16 -0300

    adding example test

---
+++ test/properties-service.spec.js
@@ -0,0 +1,18 @@
+const propertiesService = require('../lib/services/properties-service');
+const assert = require('assert');
+
+describe('properties-service', function() {
+  describe('getProperties', function() {
+    it('should return environtment variables', async () => {
+		try {
+			const serviceName = '';
+			const region = '';
+
+	
+		} catch (error) {
+			assert.fail();
+		}
+
+    });
+  });
+});
--- test/services/properties-service.spec.js
+++
@@ -1,1 +0,0 @@
-const propertiesService = require('../../lib/services/properties-service');
