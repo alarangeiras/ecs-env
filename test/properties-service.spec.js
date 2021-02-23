cb4c0b2f674d158e630ee28333cd54356c97dacd
Allan Larangeiras <alarangeiras@gmail.com>
2021-02-23 17:56:56 -0300

    adicionando prettier no projeto

--- test/properties-service.spec.js
+++ test/properties-service.spec.js
@@ -1,18 +1,15 @@
 const propertiesService = require('../lib/services/properties-service');
 const assert = require('assert');
 
-describe('properties-service', function() {
-  describe('getProperties', function() {
-    it('should return environtment variables', async () => {
-		try {
-			const serviceName = '';
-			const region = '';
-
-	
-		} catch (error) {
-			assert.fail();
-		}
-
-    });
-  });
+describe('properties-service', function () {
+	describe('getProperties', function () {
+		it('should return environtment variables', async () => {
+			try {
+				const serviceName = '';
+				const region = '';
+			} catch (error) {
+				assert.fail();
+			}
+		});
+	});
 });
